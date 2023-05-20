6fd9784a99d68247e70e06e4a5d01566b7449597
lxy1010 <3329680030@qq.com>
2023-05-20 19:45:49 +0800

    -func reset&add -bug func-read cannot work

--- main.py
+++ main.py
@@ -5,18 +5,31 @@ import csv
 class StudentManager:
     def __init__(self):
         self.filePath = 'student.csv'
-        self.head = 'ID', 'name', ''
+        self.head = self.read()[0]
 
     def clear(self):
         with open(self.filePath, 'w', newline='') as f:
             writer = csv.writer(f)
             writer.writerow(self.head)
 
-    def read(self):
+    def reset(self, head):
+        self.head = head
+        self.clear()
+
+    def read(self) -> list:
         with open(self.filePath, 'r') as f:
             reader = csv.reader(f)
-        return list(reader)
+            ls = list(reader)
+        return ls
+
+    def add(self, massage: list):
+        if len(massage) != len(self.head):
+            print('数据异常, 请重试.')
+            return
+        with open(self.filePath, 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(massage)
 
 
 if __name__ == '__main__':
-    StudentManager().clear()
+    StudentManager()
